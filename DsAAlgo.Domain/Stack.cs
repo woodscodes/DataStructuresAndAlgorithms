a1818b5ecb8f8edcefeefc85d4e0b6faf857fcb4
stuwo <stooberto@gmail.com>
2019-12-06 19:59:17 +0900

    Stack implemented in LinkedList and Array

---
+++ DsAAlgo.Domain/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsAAlgo.Domain
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public int Count { get { return _list.Count; } }
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if(_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            if(_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T value = _list.First.Value;
+            return value;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ DsAAlgo.Domain/StackAsArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsAAlgo.Domain
+{
+    public class StackAsArray<T> : IEnumerable<T>
+    {
+        private T[] _items = new T[0];
+        private int _size;
+
+        public int Count { get { return _size; } }
+        
+        public void Push(T item)
+        {
+            if(_size == _items.Length)
+            {
+                int newLength = _size == 0 ? 4 : _size * 2;
+
+                T[] newArray = new T[newLength];
+                _items.CopyTo(newArray, 0);
+                _items = newArray;
+            }
+
+            _items[_size] = item;
+            _size++;
+        }
+
+
+        public T Pop()
+        {
+            if(_size == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            _size--;
+            return _items[_size];
+        }
+
+        public T Peek()
+        {
+            if(_size == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _items[_size - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _size - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
